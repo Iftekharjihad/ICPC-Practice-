9617f3e2848391f3ae3a799cfb88dca92c1420f0
Iftekar Islam <iftekharislam923@gmail.com>
2026-03-11 21:46:48 +0600

    solved problem

--- B_Basketball_Together.cpp
+++ B_Basketball_Together.cpp
@@ -6,7 +6,22 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
       
-    
+    ll n,d;
+    cin >> n >> d;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a.rbegin(),a.rend());
+    int ans = 0,idx = 0;
+    for(int i = 0; i < n;){
+        int x = d/a[idx] + 1;
+        if(i+x <= n)ans++;
+        // ans++;
+        idx++;
+        i += x;
+    }
+    cout << ans << endl;
       
     return 0;
 }
