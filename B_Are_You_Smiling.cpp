05beb3de6b73b84edcbc7a87abc463ace6e7f0eb
Iftekar Islam <iftekharislam923@gmail.com>
2026-04-01 23:48:08 +0600

    april fool contest problem solve

---
+++ B_Are_You_Smiling.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+      
+    cout << "A2" << endl;
+      
+    return 0;
+}
