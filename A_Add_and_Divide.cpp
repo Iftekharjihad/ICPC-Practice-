a0b4151970b952361426c7bc6e61b9a65a0da701
Iftekar Islam <iftekharislam923@gmail.com>
2026-04-16 22:35:38 +0600

    divisor

--- A_Add_and_Divide.cpp
+++ A_Add_and_Divide.cpp
@@ -11,9 +11,19 @@ int main(){
     while(t--){
         ll a,b;
         cin >> a >> b;
+        int ans = INT_MAX;
         for(int i = 0; i <= 30; i++){
-            
+            ll x = a;
+            ll y = b + i;
+            if(y == 1)continue;
+            int cnt = i;
+            while(x > 0){
+                x /= y;
+                cnt++;
+            }
+            ans = min(ans,cnt);
         }
+        cout << ans << endl;
     }
     
       
