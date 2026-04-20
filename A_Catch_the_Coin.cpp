4b38a57253e85c1b774fc0cba13467163143968e
Iftekar Islam <iftekharislam923@gmail.com>
2026-04-20 23:58:07 +0600

    problem sol

---
+++ A_Catch_the_Coin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+      
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        if(y > -2)cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    
+      
+    return 0;
+}
