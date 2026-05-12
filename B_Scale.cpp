aa0f83b90812b6f56030160d621c80bcb75e6bb1
Iftekar Islam <iftekharislam923@gmail.com>
2026-05-12 23:51:27 +0600

    solved

---
+++ B_Scale.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+      
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        char a[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                cin >> a[i][j];
+            }
+        }
+        for(int i = 0; i < n; i+=k){
+            for(int j = 0; j < n; j+=k){
+                cout << a[i][j];
+            }
+        }
+        cout << endl;
+    }
+    
+      
+    return 0;
+}
